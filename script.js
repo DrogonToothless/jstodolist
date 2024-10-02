64f7f5829009a73e77f2f9fed69e4de4bde4282c
James Lawrence <jamesblawrence2007@gmail.com>
2024-10-02 10:37:28 -0600

    completed functionality of adding lists

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 function addList() {
-    const addListButton = document.getElementById("addlist");
     const listsGroup = document.getElementById("lists-group");
-    const newList = document.createElement("div");
-    const listTitle = document.createElement("input");
-    listTitle.id = "title-input";
-    listsGroup.appendChild(newList);
-    newList.appendChild(listTitle);
-    addListButton.addEventListener("keypress", addName);
-    function addName(event) {
+    const titleEntry = document.createElement("input");
+    titleEntry.type = "text";
+    listsGroup.appendChild(titleEntry);
+    titleEntry.addEventListener("keypress", createList);
+    function createList(event) {
         if (event.key === "Enter") {
-            const newTitle = listTitle.value;
-            newList.textContent = newTitle;
+            let listName = titleEntry.value.trim();
+            if (listName) {
+                let newList = document.createElement("div");
+                newList.className = "list";
+                newList.id = listName;
+                newList.textContent = listName;
+                listsGroup.appendChild(newList);
+                listsGroup.removeChild(titleEntry);
+            }
         }
     }
 }
